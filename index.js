f3ce02fe08729bf0c1f900a922f982860e65d71e
Francis <francisfrancois9189@gmail.com>
2021-11-02 14:13:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node 
+
+import fetch from "node-fetch";
+import readlineSync from "readline-sync";
+import countries from "country-list";
+
+
+const currentYear = new Date().getFullYear();
+let user = readlineSync.question('Enter a country :')
+
+const URL = "https://date.nager.at/api/v3/PublicHolidays/";
+
+if (countries.getCode(user) == undefined) {
+    do {
+        user = readlineSync.question('No valid country found. : ')
+    } while (countries.getCode(user) == undefined);
+}
+
+const countryCode = countries.getCode(user)
+const countryName = countries.getName(countryCode)
+
+
+const api = async (year, countryCode) => {
+    const response = await fetch(URL + `${year}/${countryCode}`)
+    const output = await response.json()
+
+    return output
+
+}
+
+const showItem = async () => {
+    const allHolidays = await api(currentYear, countryCode)
+    console.log("Holiday for " + countryName+'\n')
+    allHolidays.forEach(holiday => {
+
+        if (holidayFix(holiday)) {
+            console.log('\x1b[32m%s\x1b[0m', `${holiday.name}  : ${holiday.date}`)
+        }
+        else {
+            console.log('\x1b[35m%s\x1b[0m', `${holiday.name}  : ${holiday.date}`)
+        }
+
+        
+    });
+    console.log('\nColor purple is the unfixed holiday')
+    console.log('Color green is the fixed holiday')
+
+}
+
+const holidayFix = (holiday) => {
+
+    if (holiday.fixed == true) {
+        return true
+    }
+
+}
+
+console.clear()
+showItem()
+
+
+
+
